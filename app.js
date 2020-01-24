7a59d262ac8313eb1ae6afc24d10df71c778ead7
Tumurkhuyag <tomor.enkh@gmail.com>
2020-01-24 15:58:18 +0800

    Event listeners created

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-var uiController = (function() {
-  console.log("This is uiController anonymous function");
-})();
+// Дэлгэцтэй ажиллах контроллор
+var uiController = (function() {})();
 
-var financeController = (function() {
-  console.log("That is financeController anonymous function");
-})();
+// Санхүүтэй ажиллах контроллор
+var financeController = (function() {})();
 
-var appController = (function() {
-  console.log("This is appController anonymous function");
-})();
+// Програмын холбогч контроллор
+var appController = (function(uiController, financeController) {
+  var ctrlAddItem = function() {
+    console.log("Өгөгдлийг дэлгэцнээс авах хэсэг");
+    // 1. Оруулах өгөгдлийг дэлгэцнээс олж авна.
+    // 2. Олж авсан өгөгдлөө санхүүгийн контроллорт дамжуулж, тэнд хадгална
+    // 3. Олж авсан өгөгдлөө веб дээр тохирох хэёэгт нь гаргана
+    // 4. Төсвийг тооцоолно
+    // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана
+  };
+
+  document.querySelector(".add__btn").addEventListener("click", function() {
+    ctrlAddItem();
+  });
+
+  document.addEventListener("keypress", function(event) {
+    if ((event.keyCode === 13) | (event.which === 13)) {
+      ctrlAddItem();
+    }
+  });
+})(uiController, financeController);
